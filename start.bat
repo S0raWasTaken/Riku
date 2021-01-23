ddcd79c68dbb073215bbeb66ae69e64ace8037eb
S0raWasTaken <anosensedroid@gmail.com>
2021-01-23 09:37:32 -0500

    added start.bat for windows

--- start.bat
+++ start.bat
@@ -1,9 +1,4 @@
 @echo off
 cls
 SET path=%~dp0
-IF EXIST "%path%\.env" {
-    node .
-} else {
-    echo "No file .env found">%path%\errorLogs.txt
-    pause
-}
+IF EXIST "%path%\.env" node . ELSE echo "No file .env found">%path%\errorLogs.txt && pause
