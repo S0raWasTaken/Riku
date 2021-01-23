0d6445e638ecbd8b238043882818dd7fc7782a0a
S0raWasTaken <anosensedroid@gmail.com>
2021-01-23 09:38:44 -0500

    added start.bat for windows

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
 cls
 SET path=%~dp0
-IF EXIST "%path%\.env" node . ELSE echo "No file .env found">%path%\errorLogs.txt && pause
+IF EXIST "%path%\.env" ( npm i --save && node . ) ELSE ( echo "No file .env found">%path%\errorLogs.txt && pause )
