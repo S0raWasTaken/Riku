ef874f53d433eb222fa75d7cb8ce46d53efea194
S0raWasTaken <anosensedroid@gmail.com>
2021-01-23 09:54:05 -0500

    added check.bat

---
+++ check.bat
@@ -0,0 +1,14 @@
+@rem Checks if node environment is ready to run
+@echo off
+SET path=%~dp0
+
+IF EXIST "%path%\.env" (
+    IF EXIST "%path%\node_modules" (
+        echo "Ready to run node environment"
+    ) ELSE (
+        echo "Please run 'npm i --save' command"
+    )
+) ELSE (
+    echo "Please setup '.env', there's a '.env.example' file to help you setting that up"
+)
+pause
